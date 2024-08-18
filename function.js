9d2401f001b1d4a8b441ff41a988f5cab85f7ba5
ahappysloth <157324390+ahappysloth@users.noreply.github.com>
2024-08-18 11:49:49 -0400

    Update function.js

--- function.js
+++ function.js
@@ -89,7 +89,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	}
   
 	button#download {
-	  position: sticky;
+	  position: absolute;
           z-index: 10;
 	  border-radius: 0.5rem;
 	  font-size: 14px;
